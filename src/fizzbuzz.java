0b2c8e40f017d815061583e307e3bca15d284bfc
Despoina Karna <falsedk@hotmail.com>
2016-03-04 10:12:54 +0000

    Reading input from user

--- src/fizzbuzz.java
+++ src/fizzbuzz.java
@@ -1,14 +1,17 @@
 // "Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. 
 //		+ "For numbers which are multiples of both three and five print “FizzBuzz”.
+import java.util.Scanner;
 
 public class fizzbuzz {
 	
 
 	public static void main(String[] args) {
-		for (int num = 1; num<=100; num++){
-			if (num%3 == 0){
+		
+		Scanner in = new Scanner(System.in);
+			int num = in.nextInt();
+			if (num %3 == 0){
 				
-				if(num%5 == 0){
+				if(num %5 == 0){
 					System.out.println("FizzBuzz");
 				
 				}else{
@@ -19,7 +22,6 @@ public class fizzbuzz {
 			}else{
 				System.out.println(num);
 			}
-		}//for
 	
 	}//main
 	
